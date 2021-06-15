eae6a1365fcceeca4f9c4c4094a2fa82b0f52896
HPreast <preasths1@gmail.com>
2021-06-15 15:46:48 -0500

    Created templates for BlogRepository and BlogManager

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.Repositories
+{
+    public class BlogRepository : DatabaseConnector, IRepository<Blog>
+    {
+        public BlogRepository(string connectionString) : base(connectionString) { }
+
+        public List<Blog> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
