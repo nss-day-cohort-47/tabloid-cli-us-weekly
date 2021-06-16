6e87cb6d52f87cdd2c9488dba819772256d8166e
Bryson Goins <brysonmg@gmail.com>
2021-06-16 09:42:23 -0500

    need to pull main

--- TabloidCLI/UserInterfaceManagers/BlogManager.cs
+++ TabloidCLI/UserInterfaceManagers/BlogManager.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
     public class BlogManager : IUserInterfaceManager
     {
         private readonly IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepository;
+        private string _connectionString;
 
         public BlogManager(IUserInterfaceManager parentUI, string connectionString)
         {
@@ -61,7 +66,42 @@ namespace TabloidCLI.UserInterfaceManagers
 
         private void Remove()
         {
-            throw new NotImplementedException();
+            Blog blogToDelete = Choose("Whick blog would you like to remove");
+            if (blogToDelete != null)
+            {
+                _blogRepository.Delete(blogToDelete.Id);
+            }
+        }
+
+        private Blog Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose a Blog: ";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Blog> entries = _blogRepository.GetAll();
+
+            for (int i = 0; i < entries.Count; i++)
+            {
+                Journal entry = entries[i];
+                Console.WriteLine($" {i + 1}) {entry.Title}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return entries[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
         }
     }
 }
