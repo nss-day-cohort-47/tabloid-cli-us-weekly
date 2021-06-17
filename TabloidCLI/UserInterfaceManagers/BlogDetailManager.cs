d06adfd2388a2d0c7ee1bd54682fbe4ee4586d5a
HPreast <preasths1@gmail.com>
2021-06-17 13:23:01 -0500

    Implemented methods to allow users to add and delete tags from a blog on BlogDetailManager

--- TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
+++ TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
@@ -39,21 +39,21 @@ namespace TabloidCLI.UserInterfaceManagers
             {
                 case "1":
                     View();
-                    return _parentUI;
+                    return this;
                 case "2":
                     AddTag();
-                    return _parentUI;
+                    return this;
                 case "3":
                     RemoveTag();
-                    return _parentUI;
+                    return this;
                 case "4":
                     ViewPosts();
-                    return _parentUI;
+                    return this;
                 case "0":
                     return _parentUI;
                 default:
                     Console.WriteLine("Invalid Selection");
-                    return _parentUI;
+                    return this;
             }
         }
 
@@ -63,11 +63,55 @@ namespace TabloidCLI.UserInterfaceManagers
         }
         private void AddTag()
         {
-            throw new NotImplementedException();
+            Blog blog = _blogRepository.Get(_blogId);
+
+            Console.WriteLine($"Which tag would you like to add to {blog.Title}?");
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($"{i + 1}) {tag.Name}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                Tag tag = tags[choice - 1];
+                _blogRepository.InsertTag(blog, tag);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid selection, won't add any tags.");
+            }
         }
         private void RemoveTag()
         {
-            throw new NotImplementedException();
+            Blog blog = _blogRepository.Get(_blogId);
+
+            Console.WriteLine($"Which tag would you like to remove from {blog.Title}?");
+            List<Tag> tags = blog.Tags;
+
+            for (int i = 0;  i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($"{i + 1}) {tag.Name}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                Tag tag = tags[choice - 1];
+                _blogRepository.DeleteTag(blog.Id, tag.Id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid selection, won't remove any tags.");
+            }
         }
         private void ViewPosts()
         {
